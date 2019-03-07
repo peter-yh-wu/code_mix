74132ab823300b60f4ed78d481f5b38547bd85e0
peterwu99 <peter.yh.wu@gmail.com>
2019-03-07 09:39:43 -0500

    fixed bug in mfcc extractor

--- tasks/SEAME/preprocess/mk_mfcc.sh
+++ tasks/SEAME/preprocess/mk_mfcc.sh
@@ -16,11 +16,11 @@ for category in conversation interview
   cut -d ' ' -f 1 ${category}/wav.scp > speakers.${category}
   cut -d ' ' -f 1 ${category}/wav.scp > utterances.${category}
   paste -d' ' utterances.${category} speakers.${category} > ${category}/utt2spk
-  .$KALDI_DIR/utils/utt2spk_to_spk2utt.pl ${category}/utt2spk > ${category}/spk2utt
-  .$KALDI_DIR/utils/fix_data_dir.sh ${category}
+  $KALDI_DIR/utils/utt2spk_to_spk2utt.pl ${category}/utt2spk > ${category}/spk2utt
+  $KALDI_DIR/utils/fix_data_dir.sh ${category}
 
-  .$KALDI_DIR/steps/make_mfcc.sh --mfcc-config $KALDI_DIR/conf/mfcc.conf --nj 100 --cmd "run.pl" ${category} exp/mfcc mfcc_${category}
-  .$KALDI_DIR/steps/compute_cmvn_stats.sh ${category} exp/mfcc mfcc_${category}/
+  $KALDI_DIR/steps/make_mfcc.sh --mfcc-config $KALDI_DIR/conf/mfcc.conf --nj 100 --cmd "run.pl" ${category} exp/mfcc mfcc_${category}
+  $KALDI_DIR/steps/compute_cmvn_stats.sh ${category} exp/mfcc mfcc_${category}/
 
  done
 
