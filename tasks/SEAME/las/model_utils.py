b6bb14a838c6c538c3b257ed4a54a0e7c1df9cbe
peterwu99 <peter.yh.wu@gmail.com>
2019-03-08 10:53:46 -0500

    debugging rnn state

--- tasks/SEAME/las/model_utils.py
+++ tasks/SEAME/las/model_utils.py
@@ -160,7 +160,7 @@ def speech_collate_fn(batch):
     return uarray, ulens, l1array, llens, l2array
 
 def make_loader(features, labels, args, shuffle=True, batch_size=64):
-    '''
+    '''TODO
     Args:
         features: len-num_samples list
         labels: list of 1-dim int np arrays
@@ -168,5 +168,5 @@ def make_loader(features, labels, args, shuffle=True, batch_size=64):
     # Build the DataLoaders
     kwargs = {'pin_memory': True, 'num_workers': args.num_workers} if args.cuda else {}
     dataset = SpeechDataset(features, labels)
-    loader = DataLoader(dataset, collate_fn=speech_collate_fn, shuffle=shuffle, batch_size=batch_size, **kwargs)
+    loader = DataLoader(dataset, collate_fn=speech_collate_fn, shuffle=False, batch_size=batch_size, **kwargs)
     return loader
