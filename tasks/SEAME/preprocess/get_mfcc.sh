52c1ce89fb6121d19ed1ed399f4be13fd102f500
peterwu99 <peter.yh.wu@gmail.com>
2019-04-11 11:21:53 -0400

    added support for larger mfcc corpus

--- tasks/SEAME/preprocess/get_mfcc.sh
+++ tasks/SEAME/preprocess/get_mfcc.sh
@@ -5,6 +5,8 @@ mkdir -p data/interview/mfcc
 cd data/interview/mfcc
 wget http://tts.speech.cs.cmu.edu/rsk/misc_stuff/feats_seame_interview_07march2019.tar.gz
 tar -xvzf feats_seame_interview_07march2019.tar.gz
-mv feats_interview/cleaned/* .
-rm -rf feats_interview/cleaned/
+mv feats_interview/cleaned ..
+cd ..
+rm -rf mfcc
+mv cleaned mfcc
 cd ../../../preprocess
