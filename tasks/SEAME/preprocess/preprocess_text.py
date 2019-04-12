1e7a19602543f4880f7893633b31df8643b46308
peterwu99 <peter.yh.wu@gmail.com>
2019-04-12 10:16:47 -0400

    clean text

--- tasks/SEAME/preprocess/preprocess_text.py
+++ tasks/SEAME/preprocess/preprocess_text.py
@@ -18,8 +18,10 @@ from pypinyin import pinyin, lazy_pinyin, Style
 parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 INTERVIEW_TEXT_DIR = os.path.join(parent_dir, 'data/interview/transcript/phaseI')
 CONVO_TEXT_DIR = os.path.join(parent_dir, 'data/conversation/transcript/phaseI')
-INTERVIEW_PINYIN_DIR = os.path.join(parent_dir, 'data/interview/transcript_pinyin/phaseI')
-CONVO_PINYIN_DIR = os.path.join(parent_dir, 'data/conversation/transcript_pinyin/phaseI')
+# INTERVIEW_PINYIN_DIR = os.path.join(parent_dir, 'data/interview/transcript_pinyin/phaseI')
+# CONVO_PINYIN_DIR = os.path.join(parent_dir, 'data/conversation/transcript_pinyin/phaseI')
+NEW_INTERVIEW_TEXT_DIR = 'data/interview/transcript_clean/phaseI'
+NEW_CONVO_TEXT_DIR = 'data/conversation/transcript_clean/phaseI'
 
 def hanzi_to_pinyin(s):
     '''Returns string with all chinese characters turned to pinyin
@@ -57,9 +59,78 @@ def mk_pinyin_files(in_dir, out_dir):
     for inp, oup in zip(in_paths, out_paths):
         mk_pinyin_file(inp, oup)
 
+'''
 if not os.path.exists(INTERVIEW_PINYIN_DIR):
     os.makedirs(INTERVIEW_PINYIN_DIR)
 if not os.path.exists(CONVO_PINYIN_DIR):
     os.makedirs(CONVO_PINYIN_DIR)
 mk_pinyin_files(INTERVIEW_TEXT_DIR, INTERVIEW_PINYIN_DIR)
 mk_pinyin_files(CONVO_TEXT_DIR, CONVO_PINYIN_DIR)
+'''
+
+rm_list = ['[', ']', '［', '］', '`', '"', '#', '%', '\x7f', '\x84', '\x89', '\x90', '\x9d', '\x9f', '¡', '§', '·', '™', '\u3000']
+
+def clean_w(w):
+    new_w = w
+    for c in rm_list:
+        new_w = new_w.replace(c, '')
+    new_w = new_w.replace('。', '.')
+    new_w = new_w.replace('，', ',')
+    new_w = new_w.replace('．', '.')
+    new_w = new_w.replace('？', '?')
+    new_w = new_w.replace('ａ', 'a')
+    new_w = new_w.replace('ｂ', 'b')
+    new_w = new_w.replace('ｃ', 'c')
+    new_w = new_w.replace('ｄ', 'd')
+    new_w = new_w.replace('ｅ', 'e')
+    new_w = new_w.replace('ｉ', 'i')
+    new_w = new_w.replace('ｍ', 'm')
+    new_w = new_w.replace('ｎ', 'n')
+    new_w = new_w.replace('ｏ', 'o')
+    new_w = new_w.replace('ｐ', 'p')
+    new_w = new_w.replace('ｒ', 'r')
+    new_w = new_w.replace('ｓ', 's')
+    new_w = new_w.replace('ｔ', 't')
+    new_w = new_w.replace('ｕ', 'u')
+    new_w = new_w.replace('ｖ', 'v')
+    new_w = new_w.replace('=', '-')
+    new_w = new_w.replace('(ppo)', '')
+    new_w = new_w.replace('(ppc)', '')
+    new_w = new_w.replace('(ppb)', '')
+    new_w = new_w.replace('(ppl)', '')
+    new_w = new_w.replace('[ppo]', '')
+    new_w = new_w.replace('[ppc]', '')
+    new_w = new_w.replace('[ppb]', '')
+    new_w = new_w.replace('[ppl]', '')
+    return new_w
+    
+def mk_clean_file(in_path, out_path):
+    with open(in_path, 'r') as inf:
+        lines = inf.readlines()
+    new_lines = []
+    for l in lines:
+        words = l.strip().split()
+        new_words = []
+        for w in words:
+            new_words.append(clean_w(w))
+        new_lines.append(' '.join(new_words))
+    with open(out_path, 'w+') as ouf:
+        for l in new_lines:
+            # TODO ignore lines with '' as y value
+            ouf.write('%s\n' % l)
+
+def mk_clean_files(in_dir, out_dir):
+    files = os.listdir(in_dir)
+    files = [f for f in files if f.endswith('.txt')]
+    out_files = [f[:-4]+'.txt' for f in files]
+    in_paths = [os.path.join(in_dir, f) for f in files]
+    out_paths = [os.path.join(out_dir, f) for f in out_files]
+    for inp, oup in zip(in_paths, out_paths):
+        mk_clean_file(inp, oup)
+
+if not os.path.exists(NEW_INTERVIEW_TEXT_DIR):
+    os.makedirs(NEW_INTERVIEW_TEXT_DIR)
+if not os.path.exists(NEW_CONVO_TEXT_DIR):
+    os.makedirs(NEW_CONVO_TEXT_DIR)
+mk_clean_files(INTERVIEW_TEXT_DIR, NEW_INTERVIEW_TEXT_DIR)
+mk_clean_files(CONVO_TEXT_DIR, NEW_CONVO_TEXT_DIR)
