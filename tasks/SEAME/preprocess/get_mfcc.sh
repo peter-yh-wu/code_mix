a3a2c90efda8f22ca24930c210227913535f36f2
peterwu99 <peter.yh.wu@gmail.com>
2019-04-17 19:57:40 -0400

    convo phase 1 data support

--- tasks/SEAME/preprocess/get_mfcc.sh
+++ tasks/SEAME/preprocess/get_mfcc.sh
@@ -9,4 +9,13 @@ mv feats_interview/cleaned ..
 cd ..
 rm -rf mfcc
 mv cleaned mfcc
+cd ..
+mkdir -p conversation/mfcc
+cd conversation/mfcc
+wget http://tts.speech.cs.cmu.edu/rsk/misc_stuff/feats_seame_conversation_07march2019.tar.gz 
+tar -xvzf feats_seame_conversation_07march2019.tar.gz
+mv feats_conversation/cleaned ..
+cd ..
+rm -rf mfcc
+mv cleaned mfcc
 cd ../../preprocess
--- tasks/SEAME/preprocess/split_data.py
+++ tasks/SEAME/preprocess/split_data.py
@@ -17,46 +17,56 @@ SEED = 0
 TRAIN_FRAC = 0.8
 DEV_FRAC = 0.1
 
-TRAIN_IDS_FILE = 'train_ids.txt'
-DEV_IDS_FILE = 'dev_ids.txt'
-TEST_IDS_FILE = 'test_ids.txt'
+TRAIN_PATHS_FILE = 'train_paths.txt'
+DEV_PATHS_FILE = 'dev_paths.txt'
+TEST_PATHS_FILE = 'test_paths.txt'
 TRAIN_YS_FILE = 'train_ys.txt'
 DEV_YS_FILE = 'dev_ys.txt'
 TEST_YS_FILE = 'test_ys.txt'
 
 parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 INTERVIEW_MFCC_DIR = os.path.join(parent_dir, 'data/interview/mfcc')
+CONVERSATION_MFCC_DIR = os.path.join(parent_dir, 'data/conversation/mfcc')
 
 SPLIT_DIR = os.path.join(parent_dir, 'split')
 if not os.path.exists(SPLIT_DIR):
     os.makedirs(SPLIT_DIR)
-train_ids_path = os.path.join(SPLIT_DIR, TRAIN_IDS_FILE)
-dev_ids_path = os.path.join(SPLIT_DIR, DEV_IDS_FILE)
-test_ids_path = os.path.join(SPLIT_DIR, TEST_IDS_FILE)
+train_paths_path = os.path.join(SPLIT_DIR, TRAIN_PATHS_FILE)
+dev_paths_path = os.path.join(SPLIT_DIR, DEV_PATHS_FILE)
+test_paths_path = os.path.join(SPLIT_DIR, TEST_PATHS_FILE)
 train_ys_path = os.path.join(SPLIT_DIR, TRAIN_YS_FILE)
 dev_ys_path = os.path.join(SPLIT_DIR, DEV_YS_FILE)
 test_ys_path = os.path.join(SPLIT_DIR, TEST_YS_FILE)
 
-mfcc_files = os.listdir(INTERVIEW_MFCC_DIR)
-mfcc_files = [f for f in mfcc_files if f.endswith('.mfcc')]
+interview_mfcc_files = os.listdir(INTERVIEW_MFCC_DIR)
+interview_mfcc_files = [f for f in mfcc_files if f.endswith('.mfcc')]
+conversation_mfcc_files = os.listdir(CONVERSATION_MFCC_DIR)
+conversation_mfcc_files = [f for f in mfcc_files if f.endswith('.mfcc')]
 
-ids = [f[:-5] for f in mfcc_files]
+interview_paths = [os.path.join(INTERVIEW_MFCC_DIR, f) for f in interview_mfcc_files]
+conversation_paths = [os.path.join(CONVERSATION_MFCC_DIR, f) for f in conversation_mfcc_files]
+paths = interview_paths+conversation_paths
 
 random.seed(SEED)
-random.shuffle(ids)
+random.shuffle(paths)
 
-num_samples = len(ids)
+num_samples = len(paths)
 num_train = int(TRAIN_FRAC*num_samples)
 num_dev = int(DEV_FRAC*num_samples)
 
-train_ids = ids[:num_train]
-dev_ids = ids[num_train:num_train+num_dev]
-test_ids = ids[num_train+num_dev:]
+train_paths = paths[:num_train]
+dev_paths = paths[num_train:num_train+num_dev]
+test_paths = paths[num_train+num_dev:]
 
 INTERVIEW_TEXT_DIR = os.path.join(parent_dir, 'data/interview/transcript_clean/phaseI')
-txt_files = os.listdir(INTERVIEW_TEXT_DIR)
-txt_files = [f for f in txt_files if f.endswith('.txt')]
-txt_paths = [os.path.join(INTERVIEW_TEXT_DIR, f) for f in txt_files]
+CONVERSATION_TEXT_DIR = os.path.join(parent_dir, 'data/conversation/transcript_clean/phaseI')
+interview_txt_files = os.listdir(INTERVIEW_TEXT_DIR)
+interview_txt_files = [f for f in interview_txt_files if f.endswith('.txt')]
+conversation_txt_files = os.listdir(CONVERSATION_TEXT_DIR)
+conversation_txt_files = [f for f in conversation_txt_files if f.endswith('.txt')]
+interview_txt_paths = [os.path.join(INTERVIEW_TEXT_DIR, f) for f in txt_files]
+conversation_txt_paths = [os.path.join(CONVERSATION_TEXT_DIR, f) for f in txt_files]
+txt_paths = interview_txt_paths+conversation_txt_paths
 all_ys = {}
 for f in txt_paths:
     with open(f, 'r') as inf:
@@ -65,26 +75,29 @@ for f in txt_paths:
     for l in lines:
         tokens = l.split()
         fid = tokens[0]+'_'+tokens[1]+'_'+tokens[2]
+        file_name = fid+'.mfcc'
+        mfcc_dir = INTERVIEW_MFCC_DIR if 'interview' in f else CONVERSATION_MFCC_DIR
+        file_path = os.path.join(mfcc_dir, file_name)
         start_i = len(fid)+1
         y_label = l[start_i:].strip()
         if len(y_label) > 0:
-            all_ys[fid] = y_label
-train_ys = [all_ys[fid] for fid in train_ids if fid in all_ys]
-dev_ys = [all_ys[fid] for fid in dev_ids if fid in all_ys]
-test_ys = [all_ys[fid] for fid in test_ids if fid in all_ys]
+            all_ys[file_path] = y_label
+train_ys = [all_ys[path] for path in train_paths if path in all_ys]
+dev_ys = [all_ys[path] for path in dev_paths if path in all_ys]
+test_ys = [all_ys[path] for path in test_paths if path in all_ys]
 
-with open(train_ids_path, 'w+') as ouf:
-    for fid in train_ids:
-        if fid in all_ys:
-            ouf.write('%s\n' % fid)
-with open(dev_ids_path, 'w+') as ouf:
-    for fid in dev_ids:
-        if fid in all_ys:
-            ouf.write('%s\n' % fid)
-with open(test_ids_path, 'w+') as ouf:
-    for fid in test_ids:
-        if fid in all_ys:
-            ouf.write('%s\n' % fid)
+with open(train_paths_path, 'w+') as ouf:
+    for path in train_paths:
+        if path in all_ys:
+            ouf.write('%s\n' % path)
+with open(dev_paths_path, 'w+') as ouf:
+    for path in dev_paths:
+        if path in all_ys:
+            ouf.write('%s\n' % path)
+with open(test_paths_path, 'w+') as ouf:
+    for path in test_paths:
+        if path in all_ys:
+            ouf.write('%s\n' % path)
 
 with open(train_ys_path, 'w+') as ouf:
     for y in train_ys:
