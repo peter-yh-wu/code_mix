089534dbefbde37bb49d5493cd0a0e68c0f66bcc
peterwu99 <peter.yh.wu@gmail.com>
2019-04-20 10:15:30 -0400

    II speech data preprocessing

--- tasks/SEAME/preprocess/preprocess_speech.py
+++ tasks/SEAME/preprocess/preprocess_speech.py
@@ -9,12 +9,18 @@ import os
 from pydub import AudioSegment
 
 parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-INTERVIEW_TEXT_DIR = os.path.join(parent_dir, 'data/interview/transcript/phaseI')
-INTERVIEW_AUDIO_DIR = os.path.join(parent_dir, 'data/interview/audio')
-INTERVIEW_WAV_DIR = os.path.join(parent_dir, 'data/interview/wav')
-CONVO_TEXT_DIR = os.path.join(parent_dir, 'data/conversation/transcript/phaseI')
-CONVO_AUDIO_DIR = os.path.join(parent_dir, 'data/conversation/audio')
-CONVO_WAV_DIR = os.path.join(parent_dir, 'data/conversation/wav')
+INTERVIEW_TEXT_DIR_I = os.path.join(parent_dir, 'data/interview/transcript/phaseI')
+INTERVIEW_AUDIO_DIR_I = os.path.join(parent_dir, 'data/interview/audioI')
+INTERVIEW_WAV_DIR_I = os.path.join(parent_dir, 'data/interview/wavI')
+CONVO_TEXT_DIR_I = os.path.join(parent_dir, 'data/conversation/transcript/phaseI')
+CONVO_AUDIO_DIR_I = os.path.join(parent_dir, 'data/conversation/audioI')
+CONVO_WAV_DIR_I = os.path.join(parent_dir, 'data/conversation/wavI')
+INTERVIEW_TEXT_DIR_II = os.path.join(parent_dir, 'data/interview/transcript/phaseII')
+INTERVIEW_AUDIO_DIR_II = os.path.join(parent_dir, 'data/interview/audioII')
+INTERVIEW_WAV_DIR_II = os.path.join(parent_dir, 'data/interview/wavII')
+CONVO_TEXT_DIR_II = os.path.join(parent_dir, 'data/conversation/transcript/phaseII')
+CONVO_AUDIO_DIR_II = os.path.join(parent_dir, 'data/conversation/audioII')
+CONVO_WAV_DIR_II = os.path.join(parent_dir, 'data/conversation/wavII')
 
 def crop_wav(in_path, out_path, t1, t2):
     '''t1 and t2 are in milliseconds, assumes both paths are .wav files'''
@@ -52,9 +58,17 @@ def crop_data(txt_dir, audio_dir, wav_dir):
             t2s.append(t2)
         crop_wavs(audio_path, wav_paths, t1s, t2s)
 
-if not os.path.exists(INTERVIEW_WAV_DIR):
-    os.makedirs(INTERVIEW_WAV_DIR)
-if not os.path.exists(CONVO_WAV_DIR):
-    os.makedirs(CONVO_WAV_DIR)
-crop_data(INTERVIEW_TEXT_DIR, INTERVIEW_AUDIO_DIR, INTERVIEW_WAV_DIR)
-crop_data(CONVO_TEXT_DIR, CONVO_AUDIO_DIR, CONVO_WAV_DIR)
+'''
+if not os.path.exists(INTERVIEW_WAV_DIR_I):
+    os.makedirs(INTERVIEW_WAV_DIR_I)
+if not os.path.exists(CONVO_WAV_DIR_I):
+    os.makedirs(CONVO_WAV_DIR_I)
+crop_data(INTERVIEW_TEXT_DIR_I, INTERVIEW_AUDIO_DIR_I, INTERVIEW_WAV_DIR_I)
+crop_data(CONVO_TEXT_DIR_I, CONVO_AUDIO_DIR_I, CONVO_WAV_DIR_I)
+'''
+if not os.path.exists(INTERVIEW_WAV_DIR_II):
+    os.makedirs(INTERVIEW_WAV_DIR_II)
+if not os.path.exists(CONVO_WAV_DIR_II):
+    os.makedirs(CONVO_WAV_DIR_II)
+crop_data(INTERVIEW_TEXT_DIR_II, INTERVIEW_AUDIO_DIR_II, INTERVIEW_WAV_DIR_II)
+crop_data(CONVO_TEXT_DIR_II, CONVO_AUDIO_DIR_II, CONVO_WAV_DIR_II)
