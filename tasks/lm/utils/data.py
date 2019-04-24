4cb90ea2ccdc29a509a9bbbb3177f2a4302862b3
zimengq <zimengq@andrew.cmu.edu>
2019-04-24 13:51:37 -0400

    modify read dataset

--- tasks/lm/utils/data.py
+++ tasks/lm/utils/data.py
@@ -5,17 +5,13 @@
 # Licensed under the Apache License v2.0 - http://www.apache.org/licenses/
 
 import os
-<<<<<<< HEAD
-
-=======
 import re
 import multiprocessing as mp
 from glob import glob
->>>>>>> 8ca1d760cbb5966d0f96505b071a427524ca2211
 
-def extract_files_data(file_paths):
+
+def extract_files_data(data_path):
     data = []
-<<<<<<< HEAD
     for (dirpath, dirs, files) in os.walk(data_path):
         for file in files:
             with open(os.path.join(dirpath, file), "r") as f:
@@ -38,16 +34,12 @@ def extract_files_data(file_paths):
                                     tmp += char
                     assert(all(len(word) == 1 for word in text if is_chinese_word(word)))
                     data.append([word.encode('UTF-8') for word in text])
-=======
-    for file_path in file_paths:
-        with open(file_path, "r") as f:
-            text = f.readlines()
-            data.extend([[word for word in line.strip().split(" ")[3:] if word != '<v-noise>'] for line in text])
     return data
+
 
 def read_dataset(data_path, num_workers=1):
     data = []
-    all_file_paths = glob(os.path.join(data_path, '**/*.txt'), recursive=True)
+    all_file_paths = glob(os.path.join(dfiata_path, '**/*.txt'), recursive=True)
     num_files = len(all_file_paths)
     files_per_worker = num_files // num_workers
 
@@ -59,7 +51,6 @@ def read_dataset(data_path, num_workers=1):
 
     for result in extraction_result:
         data.extend(result)
->>>>>>> 8ca1d760cbb5966d0f96505b071a427524ca2211
     return data
 
 
@@ -76,7 +67,10 @@ def is_english_word(word):
     :param word: A token in document.
     :return: Boolean value, True or False
     """
-    return all([char in ["\"", "\'", "-"] or char.isalpha() for char in word]) and len(word) > 0
+    return all([char in ["\"", "\'", "-", "*", "~", "*", "."] or char.isalpha() for char in word])
+
+def has_chinese_char(word):
+    return len(re.findall(r'[\u4e00-\u9fff]+', word)) > 0
 
 
 def is_chinese_word(char):
@@ -86,10 +80,15 @@ def is_chinese_word(char):
         {"from": ord(u"\u3300"), "to": ord(u"\u33ff")},  # compatibility ideographs
         {"from": ord(u"\ufe30"), "to": ord(u"\ufe4f")},  # compatibility ideographs
         {"from": ord(u"\uf900"), "to": ord(u"\ufaff")},  # compatibility ideographs
+        {"from": ord(u"\U0002F800"), "to": ord(u"\U0002fa1f")},  # compatibility ideographs
         {'from': ord(u'\u3040'), 'to': ord(u'\u309f')},  # Japanese Hiragana
         {"from": ord(u"\u30a0"), "to": ord(u"\u30ff")},  # Japanese Katakana
         {"from": ord(u"\u2e80"), "to": ord(u"\u2eff")},  # cjk radicals supplement
         {"from": ord(u"\u4e00"), "to": ord(u"\u9fff")},
         {"from": ord(u"\u3400"), "to": ord(u"\u4dbf")},
+        {"from": ord(u"\U00020000"), "to": ord(u"\U0002a6df")},
+        {"from": ord(u"\U0002a700"), "to": ord(u"\U0002b73f")},
+        {"from": ord(u"\U0002b740"), "to": ord(u"\U0002b81f")},
+        {"from": ord(u"\U0002b820"), "to": ord(u"\U0002ceaf")}  # included as of Unicode 8.0
     ]
     return any([range["from"] <= ord(char) <= range["to"] for range in ranges])
