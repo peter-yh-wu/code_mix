4cc0200468071cabc50ac8eda15664c339eccd83
salmedina <salbed.medina@gmail.com>
2019-04-27 10:56:33 -0400

    Update test_model from baseline

--- tasks/SEAME/beam_las/test_model.py
+++ tasks/SEAME/beam_las/test_model.py
@@ -30,36 +30,79 @@ def main():
 
     t0 = time.time()
 
+    if not os.path.exists(args.save_directory):
+        os.makedirs(args.save_directory)
+    LOG_PATH = os.path.join(args.save_directory, 'log')
+    with open(LOG_PATH, 'w+') as ouf:
+        pass
+
+    print("Loading File Paths")
     train_paths, dev_paths, test_paths = load_paths()
     train_paths, dev_paths, test_paths = train_paths[:args.max_train], dev_paths[:args.max_dev], test_paths[:args.max_test]
+    t1 = time.time()
+    print_log('%.2f Seconds' % (t1-t0), LOG_PATH)
 
+    print("Loading Y Data")
     test_paths = test_paths[:args.max_data]
     train_ys = load_y_data('train') # 1-dim np array of strings
     dev_ys = load_y_data('dev')
     test_ys = load_y_data('test')
+    t1 = time.time()
+    print_log('%.2f Seconds' % (t1-t0), LOG_PATH)
 
+    print("Building Charset")
     charset = build_charset(np.concatenate((train_ys, dev_ys, test_ys), axis=0))
     charmap = make_charmap(charset) # {string: int}
     charcount = len(charset)
+    t1 = time.time()
+    print_log('%.2f Seconds' % (t1-t0), LOG_PATH)
 
+    print("Mapping Characters")
     testchars = map_characters(test_ys, charmap)
-    test_loader = make_loader(test_paths, testchars, args, shuffle=False, batch_size=1)
+    print("Building Loader")
+    test_loader = make_loader(test_paths, testchars, args, shuffle=False, batch_size=args.batch_size)
 
+    print("Building Model")
     model = Seq2SeqModel(args, vocab_size=charcount)
 
     CKPT_PATH = os.path.join(args.save_directory, 'model.ckpt')
-    model.load_state_dict(torch.load(CKPT_PATH))
+    if args.cuda:
+        model.load_state_dict(torch.load(CKPT_PATH))
+    else:
+        gpu_dict = torch.load(CKPT_PATH, map_location=lambda storage, loc: storage)
+        cpu_model_dict = {}
+        for key, val in gpu_dict.items():
+            cpu_model_dict[key] = val.cpu()
+        model.load_state_dict(cpu_model_dict)
+    print("Loaded Checkpoint")
+
     if args.cuda:
         model = model.cuda()
     
     model.eval()
-    write_transcripts(
-        path=os.path.join(args.save_directory, 'submission.csv'),
-        args=args, model=model, loader=test_loader, charset=charset
-    )
+    CSV_PATH = os.path.join(args.save_directory, 'submission.csv')
+    if not os.path.exists(CSV_PATH):
+        transcripts = write_transcripts(
+            path=CSV_PATH,
+            args=args, model=model, loader=test_loader, charset=charset
+        )
+    else:
+        transcripts = []
+        with open(CSV_PATH, 'r') as csvfile:
+            raw_csv = csv.reader(csvfile)
+            for row in raw_csv:
+                transcripts.append(row[1])
     t1 = time.time()
+    print("Finshed Writing Transcripts")
     print('%.2f Seconds' % t1-t0)
-    # TODO perplexity, CER and text
+    
+    CER_PATH = os.path.join(args.save_directory, 'test_cer.npy')
+    norm_dists = cer_from_transcripts(transcripts, test_ys)
+    np.save(CER_PATH, norm_dists)
+
+    PERP_PATH = os.path.join(args.save_directory, 'test_perp.npy')
+    all_perps = perplexities_from_x(model, test_loader)
+    np.save(PERP_PATH, all_perps)
 
 if __name__ == '__main__':
     main()
