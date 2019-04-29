ed80a5cc79952fbeefb69aea22f04b933d1af939
zimengq <zimengq@andrew.cmu.edu>
2019-04-29 17:00:37 -0400

    fix bugs

--- tasks/SEAME/beam_las/model_utils.py
+++ tasks/SEAME/beam_las/model_utils.py
@@ -108,7 +108,7 @@ def decode_output(output, charset):
         chars.append(charset[o - 1])
     return "".join(chars)
 
-def generate_transcripts(args, model, loader, charset, lm):
+def generate_transcripts(args, model, loader, charset):
     '''Iteratively returns string transcriptions
     
     Return:
