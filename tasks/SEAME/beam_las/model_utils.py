d8a65eeba928ebb608a00b0652eec102a2bbd6e1
salmedina <salbed.medina@gmail.com>
2019-04-29 21:26:39 -0400

    Fix bug transcript not working with beam < 1

--- tasks/SEAME/beam_las/model_utils.py
+++ tasks/SEAME/beam_las/model_utils.py
@@ -129,7 +129,7 @@ def generate_transcripts(args, model, loader, charset):
         logits, generated, lens = model(uarray, ulens, l1array, llens)
         generated = generated.data.cpu().numpy()  # (L, BS)
         n = uarray.size(1)
-        for i in range(model.beam_width):
+        for i in range(min(1, model.beam_width)):
             transcript = decode_output(generated[:, i], charset)
             yield transcript
 
