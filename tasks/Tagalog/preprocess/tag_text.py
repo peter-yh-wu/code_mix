6e2264e98964a7efa740588fb5844b3ab9e28c9c
peter-yh-wu <peter.yh.wu@gmail.com>
2019-06-25 11:50:25 -0400

    switched to langid lib

--- tasks/Tagalog/preprocess/tag_text.py
+++ tasks/Tagalog/preprocess/tag_text.py
@@ -1,6 +1,5 @@
+import langid
 import os
-
-from googletrans import Translator
 
 def main():
     parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -15,22 +14,22 @@ def main():
     with open(scripted_txt_path, 'r') as inf:
         script_lines = inf.readlines()
 
-    translator = Translator()
-
     new_lines = []
-    for l in script_lines:
+    for i, l in enumerate(script_lines):
         l = l.strip()
         l_list = l.split()
 
         lids = []
         for w in l_list[1:]:
-            t = translator.translate(w)
-            lid = t.src # eg 'tl'
+            lid = langid.classify('kaarawan')[0]
             lids.append(lid)
 
         new_l_list = [l_list[0]]+lids
         new_l = ' '.join(new_l_list)
         new_lines.append(new_l)
+
+        if (i+1) % 1000 == 0:
+            print('Processed %d / %d lines' % (i+1, len(script_lines)))
 
     with open(scripted_lids_path, 'w+') as ouf:
         for l in new_lines:
