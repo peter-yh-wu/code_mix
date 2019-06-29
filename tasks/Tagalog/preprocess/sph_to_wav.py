6272d2fc3ed87ed5b3a38cba94a385e090d5d3f0
peter-yh-wu <peter.yh.wu@gmail.com>
2019-06-29 12:24:56 -0400

    sph to wav conv

--- tasks/Tagalog/preprocess/sph_to_wav.py
+++ tasks/Tagalog/preprocess/sph_to_wav.py
@@ -6,18 +6,18 @@ import wave
 def main():
   parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
   data_dir = os.path.join(parent_dir, 'data')
-  scripted_audio_dir = os.path.join(data_dir, 'scripted/audio')
+  audio_dir = os.path.join(data_dir, 'conversational/audio') # 'scripted/audio')
 
-  scripted_audio_files = os.listdir(scripted_audio_dir)
-  scripted_audio_files = [f for f in scripted_audio_files if f.endswith('.sph')]
-  scripted_audio_paths = [os.path.join(scripted_audio_dir, f) for f in scripted_audio_files]
+  audio_files = os.listdir(audio_dir)
+  audio_files = [f for f in audio_files if f.endswith('.sph')]
+  audio_paths = [os.path.join(audio_dir, f) for f in audio_files]
 
   wav_dir = os.path.join(data_dir, 'wav')
   if not os.path.exists(wav_dir):
     os.makedirs(wav_dir)
 
-  for i, p in enumerate(scripted_audio_paths):
-    curr_audio_file = scripted_audio_files[i]
+  for i, p in enumerate(audio_paths):
+    curr_audio_file = audio_files[i]
     print(curr_audio_file)
     curr_wav_file = curr_audio_file[:-4]+'.wav'
     curr_wav_path = os.path.join(wav_dir, curr_wav_file)
