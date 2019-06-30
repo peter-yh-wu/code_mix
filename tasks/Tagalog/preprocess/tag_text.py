404fc38fd3c90435c945ad2a8919431f91e82555
peter-yh-wu <peter.yh.wu@gmail.com>
2019-06-29 21:44:50 -0400

    tags all txt files in text directory

--- tasks/Tagalog/preprocess/tag_text.py
+++ tasks/Tagalog/preprocess/tag_text.py
@@ -1,3 +1,7 @@
+'''
+Creates language id files for all text files in text directory
+'''
+
 import enchant
 # import langid
 import os
@@ -8,45 +12,51 @@ def main():
     parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     data_dir = os.path.join(parent_dir, 'data')
     txt_dir = os.path.join(data_dir, 'txt')
-    # scripted_txt_path = os.path.join(txt_dir, 'script.txt')
-    conv_txt_path = os.path.join(txt_dir, 'conv.txt')
+    txt_files = os.listdir(txt_dir)
+    txt_files = [f for f in txt_files if f.endswith('.txt')]
+    txt_paths = [os.path.join(txt_dir, f) for f in txt_files]
+
     lids_dir = os.path.join(data_dir, 'lids')
     if not os.path.exists(lids_dir):
         os.makedirs(lids_dir)
-    # scripted_lids_path = os.path.join(lids_dir, 'script_lids.txt')
-    conv_lids_path = os.path.join(lids_dir, 'conv_lids.txt')
-
-    with open(conv_txt_path, 'r') as inf: # scripted_txt_path, 'r') as inf:
-        conv_lines = inf.readlines() # script_lines = inf.readlines()
 
     d = enchant.Dict("en_US")
 
-    new_lines = []
-    for i, l in enumerate(conv_lines): # script_lines):
-        l = l.strip()
-        l_list = l.split()
+    for file_i, txt_path in enumerate(txt_paths):
+        txt_file = txt_files[file_i]
+        fid = txt_file[:-4]
+        txt_file = fid+'_lids.txt'
+        lids_path = os.path.join(lids_dir, txt_file)
 
-        lids = []
-        for w in l_list[1:]:
-            if d.check(w):
-                lid = 'en'
-            else:
-                # lid1 = langid.classify(w)[0]
-                # lid2 = detect(w)
-                # lid = lid2
-                lid = 'tl'
-            lids.append(lid)
+        with open(txt_path, 'r') as inf:
+            lines = inf.readlines()
 
-        new_l_list = [l_list[0]]+lids
-        new_l = ' '.join(new_l_list)
-        new_lines.append(new_l)
+        new_lines = []
+        for line_i, l in enumerate(lines):
+            l = l.strip()
+            l_list = l.split()
 
-        if (i+1) % 1000 == 0:
-            print('Processed %d / %d lines' % (i+1, len(conv_lines))) # len(script_lines)))
+            lids = []
+            for w in l_list[1:]:
+                if d.check(w):
+                    lid = 'en'
+                else:
+                    # lid1 = langid.classify(w)[0]
+                    # lid2 = detect(w)
+                    # lid = lid2
+                    lid = 'tl'
+                lids.append(lid)
 
-    with open(conv_lids_path, 'w+') as ouf: # scripted_lids_path, 'w+') as ouf:
-        for l in new_lines:
-            ouf.write('%s\n' % l)
+            new_l_list = [l_list[0]]+lids
+            new_l = ' '.join(new_l_list)
+            new_lines.append(new_l)
+
+            if (line_i+1) % 1000 == 0:
+                print('File %d: Processed %d / %d lines' % (file_i+1, line_i+1, len(lines)))
+
+        with open(lids_path, 'w+') as ouf:
+            for l in new_lines:
+                ouf.write('%s\n' % l)
 
 if __name__ == "__main__":
     main()
