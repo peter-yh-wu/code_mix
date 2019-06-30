5f9b2dcfcd263e6671c902a4700a021989fcf53c
zimengq <zimengq@andrew.cmu.edu>
2019-06-30 00:05:01 -0400

    save generated sentences to local file

--- tasks/lm/train.py
+++ tasks/lm/train.py
@@ -36,7 +36,7 @@ def calc_sent_loss(sent, model, criterion, lang_ids=None):
         loss += criterion(lang_ids_pred, lang_ids[1:])
 
     gen_sent = ' '.join([model.vocab[idx] for idx in torch.argmax(logits, dim=1)])
-    with open('log/{}_gen_sent.txt'.format(args.dataset), 'w') as f:
+    with open('log/{}_gen_sent.txt'.format(args.dataset), 'w+') as f:
         f.write(gen_sent)
     return loss
 
