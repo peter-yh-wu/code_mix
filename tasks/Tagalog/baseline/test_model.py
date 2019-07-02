457d283684f36a41aea0f3f325da3734e74788b3
peter-yh-wu <peter.yh.wu@gmail.com>
2019-07-02 11:48:34 -0400

    fixed write transcript bug

--- tasks/Tagalog/baseline/test_model.py
+++ tasks/Tagalog/baseline/test_model.py
@@ -4,7 +4,7 @@ Script to evaluate model
 Assumes that model.ckpt exists
 Supported test-mode values: transcript, cer, perp, and all combos
 
-e.g. to calculate CER - python3 test_model.py --test-mode transcript cer
+e.g. to calculate CER - python3 test_model.py --test-mode transcript_cer
     Make sure to also add the same args used when running main.py
 
 Peter Wu
