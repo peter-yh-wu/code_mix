5fbcfd28a9551a6261b72416a59fa37e33353395
peter-yh-wu <peter.yh.wu@gmail.com>
2019-07-04 02:51:39 -0400

    bincount expt

--- tasks/Tagalog/discr/g2p2g.py
+++ tasks/Tagalog/discr/g2p2g.py
@@ -42,7 +42,7 @@ def main():
 
     print(max(lens), sum(lens)/len(lens))
     distr = np.bincount(lens)/len(lens)
-    print(np.random.multinomial(1, distr).where(array==item))
+    print(np.where(np.random.multinomial(1, distr)==1))
 
 if __name__ == '__main__':
     main()
