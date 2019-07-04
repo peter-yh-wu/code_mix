b5724d1cd7eaee5840ab520aa97604c224c2374f
zimengq <zimengq@andrew.cmu.edu>
2019-07-03 21:25:55 -0400

    fix bug

--- tasks/lm/lm.py
+++ tasks/lm/lm.py
@@ -107,7 +107,7 @@ class DualLSTM(nn.Module):
 
     def embed_sentence(self, sentence, lang_ids=None):
         embedding = []
-        if self.dataset == 'seame':
+        if self.dataset == 'seame' or self.dataset == 'qg':
             embed_mask = torch.zeros(len(sentence))
             for idx, token in enumerate(sentence[:-1]):
                 try:
